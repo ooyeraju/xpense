d6acec8703461534caf46f43bacb56e9d6aa9bbe
ooyeraju <rajnishkush2@gmail.com>
2020-09-30 23:44:20 +0530

    added shared pref

--- lib/models/transactions.dart
+++ lib/models/transactions.dart
@@ -1,16 +1,26 @@
-import 'package:flutter/foundation.dart';
-
 class Transaction {
   final String id;
   final String title;
   final int amount;
   final DateTime date;
 
-
   Transaction({
-    @required this.id, 
-    @required this.title, 
-    @required this.amount, 
-    @required this.date
-    });
+    this.id,
+    this.title,
+    this.amount,
+    this.date,
+  });
+
+  Transaction.fromJson(Map<dynamic, dynamic> json)
+      : id = json['id'],
+        title = json['title'],
+        amount = json['amount'],
+        date = json['date'];
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'title': title,
+        'amount': amount,
+        'date': date,
+      };
 }
